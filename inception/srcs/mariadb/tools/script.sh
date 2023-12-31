a01896601ab9c3579d470176b1a13154a46c49af
khalid bouychou <khalidbouychou22@gmail.com>
2023-12-31 14:00:01 +0100

    nginx mariadb done

--- inception/srcs/mariadb/tools/script.sh
+++ inception/srcs/mariadb/tools/script.sh
@@ -6,11 +6,15 @@ S_PWD="0000"
 S_ROOT_P="1111"
 
 service mariadb start
-sleep 2
+
+sleep 5
+
 mysql -u root -p$S_ROOT_P -e "CREATE DATABASE IF NOT EXISTS \`${SDB}\`;"
-mysql -u root -p$S_ROOT_P -e "CREATE USER IF NOT EXISTS \`${S_USER}\`@'localhost' IDENTIFIED BY '${S_PWD}';"
+mysql -u root -p$S_ROOT_P -e "CREATE USER IF NOT EXISTS \`${S_USER}\`@'%' IDENTIFIED BY '${S_PWD}';"
 mysql -u root -p$S_ROOT_P -e "GRANT ALL PRIVILEGES ON \`${SDB}\`.* TO \`${S_USER}\`@'%';"
 mysql -u root -p$S_ROOT_P -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${S_ROOT_P}';"
 mysql -u root -p$S_ROOT_P -e "FLUSH PRIVILEGES;"
+
 mysqladmin -u root -p$S_ROOT_P shutdown
+
 exec mysqld_safe
