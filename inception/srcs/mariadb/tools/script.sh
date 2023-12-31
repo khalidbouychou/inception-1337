6b983a67ad1d058275ad29cf444a52cb2c29fc3a
khalid bouychou <khalidbouychou22@gmail.com>
2023-12-31 16:35:10 +0100

    still wordpress not working

--- inception/srcs/mariadb/tools/script.sh
+++ inception/srcs/mariadb/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SDB="Sdb"
-S_USER="adil"
+SDB="sdb"
+S_USER="khbouych"
 S_PWD="0000"
 S_ROOT_P="1111"
 
@@ -9,10 +9,10 @@ service mariadb start
 
 sleep 5
 
-mysql -u root -p$S_ROOT_P -e "CREATE DATABASE IF NOT EXISTS \`${SDB}\`;"
-mysql -u root -p$S_ROOT_P -e "CREATE USER IF NOT EXISTS \`${S_USER}\`@'%' IDENTIFIED BY '${S_PWD}';"
-mysql -u root -p$S_ROOT_P -e "GRANT ALL PRIVILEGES ON \`${SDB}\`.* TO \`${S_USER}\`@'%';"
-mysql -u root -p$S_ROOT_P -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${S_ROOT_P}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SDB}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${S_USER}\`@'%' IDENTIFIED BY '${S_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${S_USER}\`@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${S_ROOT_P}';"
 mysql -u root -p$S_ROOT_P -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$S_ROOT_P shutdown
---
+++ inception/srcs/wordpress/conf/auto_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar  
+
+chmod  755 /var/www/wordpress/ 
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/wordpress/
+
+# wp core download --allow-root
+
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+
+sed -i '36 s/\/run\/php\/php7.4-fpm.sock/9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+wp config set --allow-root DB_NAME ${SDB} --path='/var/www/wordpress'
+wp config set --allow-root DB_USER ${S_USER} --path='/var/www/wordpress'
+wp config set --allow-root DB_PASSWORD ${S_PWD} --path='/var/www/wordpress'
+wp config set --allow-root DB_HOST "mariadb:3306" --path='/var/www/wordpress'
+
+# wp user create --allow-root hello hell@gmail.com --path='/var/www/wordpress'
+
+mkdir -p /run/php
+
+/usr/sbin/php-fpm7.4 -F
