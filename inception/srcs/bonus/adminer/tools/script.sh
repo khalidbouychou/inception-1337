17d0a490b2e0237cc78d4696f1bcb323d6326faa
khalid bouychou <khalidbouychou22@gmail.com>
2024-01-02 00:21:19 +0100

    adminer still need to fix

---
+++ inception/srcs/bonus/adminer/tools/script.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+wget "http://www.adminer.org/latest.php" -O /var/www/wordpress/adminer.php
+
+chown -R www-data:www-data /var/www/wordpress/adminer.php 
+
+chmod 755 /var/www/wordpress/adminer.php
+
+cd /var/www/wordpress
+
+rm -rf index.html
+
+php -S 0.0.0.0:80
